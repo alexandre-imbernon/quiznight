f7cc19516eac38a3cda3251a8befce1a6b750d8d
alexandre-imbernon <alexandre.imbernon@laplateforme.io>
2024-06-11 10:04:05 +0200

    Merging of Adels work and db, now questions are displayed in the quizzes page and also displayed on the index

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 include 'config.php';
 
-$stmt = $conn->query("SELECT * FROM quizzes");
-$quizzes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Récupérer la liste des quiz existants 
+$stmt = $conn->prepare("SELECT quizzes.id AS quiz_id, quizzes.title AS quiz_title, questions.id AS question_id, questions.question_text, answers.id AS answer_id, answers.answer_text, answers.is_correct FROM quizzes JOIN questions ON quizzes.id = questions.quiz_id JOIN answers ON questions.id = answers.question_id");
+$stmt->execute();
+$quiz_questions_answers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Organiser les données par quiz
+$quizzes_data = [];
+
+foreach ($quiz_questions_answers as $qa) {
+    $quizzes_data[$qa['quiz_id']]['title'] = $qa['quiz_title'];
+    $quizzes_data[$qa['quiz_id']]['questions'][$qa['question_id']]['question_text'] = $qa['question_text'];
+    $quizzes_data[$qa['quiz_id']]['questions'][$qa['question_id']]['answers'][] = [
+        'answer_text' => $qa['answer_text'],
+        'is_correct' => $qa['is_correct']
+    ];
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,9 +29,29 @@ $quizzes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <body>
     <h1>Quiz Index</h1>
     <ul>
-        <?php foreach ($quizzes as $quiz): ?>
-            <li><?php echo htmlspecialchars($quiz['title']); ?></li>
+        <?php foreach ($quizzes_data as $quiz_id => $quiz): ?>
+            <li>
+                <h3><?php echo htmlspecialchars($quiz['title']); ?></h3>
+                <ul>
+                    <?php foreach ($quiz['questions'] as $question): ?>
+                        <li>
+                            <strong><?php echo htmlspecialchars($question['question_text']); ?></strong><br>
+                            <ul>
+                                <?php foreach ($question['answers'] as $answer): ?>
+                                    <li>
+                                        <?php echo htmlspecialchars($answer['answer_text']); ?>
+                                        <?php if ($answer['is_correct']): ?>
+                                            (Correct)
+                                        <?php endif; ?>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </li>
         <?php endforeach; ?>
     </ul>
 </body>
 </html>
+
